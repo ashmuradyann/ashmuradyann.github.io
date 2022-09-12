c548ffb02de81872cf1b82aac357c7b5acf72a54
ashmuradyann <ashot.muradyan16@gmail.com>
2022-09-12 22:25:20 +0300

    fix

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,7 @@ const downloadButtonsWrapper = document.querySelector(".download__resume")
 const message = document.querySelector(".message")
 const likeBtns = document.querySelector(".like__btns")
 const likeCount = document.querySelector(".like__count")
+const visitedCount = document.querySelector(".visited__count")
 const likeTitleWrapper = document.querySelector(".like__title__wrapper")
 
 window.onscroll = () => {
@@ -13,32 +14,27 @@ window.onscroll = () => {
 	document.querySelector("#progress").style.transition = ".4s"
 }
 
-var typed = new Typed('#welcome',
-	{
-		strings: [
-			"Hi !",
-			"Hi^1000, I am Ashot Muradyan^1500",
-			"I am a React JS Developer^1500",
-			"So..^500 Let's Dive In !"],
-		typeSpeed: 50,
-		backSpeed: 35,
-		startDelay: 500,
-		onComplete: (self) => {
-			let welcome = document.querySelector(".welcome__wrapper")
-			let arrow = document.querySelector(".arrow__wrapper")
-			arrow.style.opacity = "1"
-			arrow.style.transform = "translateY(-10px)"
-			welcome.style.transform = "translateY(0px)"
-		}
-	})
+var typed = new Typed('#welcome', {
+	strings: [
+		"Hi !",
+		"Hi^1000, I am Ashot Muradyan^1500",
+		"I am a React JS Developer^1500",
+		"So..^500 Let's Dive In !"],
+	typeSpeed: 50,
+	backSpeed: 35,
+	startDelay: 500,
+	onComplete: (self) => {
+		let welcome = document.querySelector(".welcome__wrapper")
+		let arrow = document.querySelector(".arrow__wrapper")
+		arrow.style.opacity = "1"
+		arrow.style.transform = "translateY(-10px)"
+		welcome.style.transform = "translateY(0px)"
+	}
+})
 
 const elementInView = (el, dividend = 1) => {
 	const elementTop = el.getBoundingClientRect().top;
-
-	return (
-		elementTop <=
-		(window.innerHeight || document.documentElement.clientHeight) / dividend
-	);
+	return (elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend);
 };
 
 const elementOutofView = (el) => {
@@ -94,44 +90,64 @@ const copy = () => {
 	}, 4000)
 }
 
-let count
-let KEY = "$2b$10$.cM.jejY.OkO4SvcEUU.F.WaQuAl8SF3ewJoAOZKb6ZO.3ChhgjVW"
+// let count
+// let KEY = "$2b$10$.cM.jejY.OkO4SvcEUU.F.WaQuAl8SF3ewJoAOZKb6ZO.3ChhgjVW"
 
-const like = async (state) => {
+// const like = async (state) => {
 
-	localStorage.setItem("liked", state)
-	likeBtns.innerHTML = `<img src='https://img.icons8.com/material-${state ? 'rounded' : 'outlined'}/50/ffffff/facebook-like--v1.svg' onclick='like(${state ? false : true})' />`
-	likeTitleWrapper.innerHTML = `<h2 class="like__title" data-title="${state ? 'already liked' : 'sure, like!'}">Like my portfolio site</h2>`
-	// likeCount.innerHTML = state ? count + 1 + " likes" : count == undefined ? count - 1 : count + " likes"
-	
-	await fetch("https://api.jsonbin.io/v3/b/631df3ec5c146d63ca979518", {
-		method: 'PUT',
-		headers: {
-			"Content-Type": "application/json",
-			"X-Master-Key": KEY
-		},
-		body: JSON.stringify({ "count": state ? count + 1 : count - 1 })
-	}).then(res => res.json()).then(res => console.log(res))
-}
+// 	localStorage.setItem("liked", state)
 
-likeTitleWrapper.innerHTML = `<h2 class="like__title" data-title="${localStorage.getItem("liked") === "false" ? 'sure, like!' : 'already liked'}">Like my portfolio site</h2>`
+// 	likeBtns.innerHTML = `<i class="fa-${state ? 'solid' : 'regular'} fa-heart like__btn" onclick='like(${state ? false : true})'></i>`
+// 	likeTitleWrapper.innerHTML = `<h2 class="like__title" data-title="${state ? 'already liked' : 'sure, like!'}">Like my portfolio site</h2>`
 
-window.addEventListener("load", async () => {
-	localStorage.setItem("liked", localStorage.getItem("liked") == null ? false : localStorage.getItem("liked"))
-	console.log(localStorage.getItem("liked"))
-	const headers = {
-		"X-Master-Key": KEY
-	}
-	await fetch("https://api.jsonbin.io/v3/b/631df3ec5c146d63ca979518/latest", { headers })
-		.then(res => res.json()).then(res => {
-			count = res.record.count
-		})
+// 	await fetch("https://api.jsonbin.io/v3/b/631df3ec5c146d63ca979518", {
+// 		method: 'PUT',
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 			"X-Master-Key": KEY
+// 		},
+// 		body: JSON.stringify({ "count": state ? count + 1 : count - 1 })
+// 	}).then(res => res.json()).then(res => console.log(res))
+// }
 
-	likeCount.innerHTML = count + " likes"
-	
-	if (localStorage.getItem("liked") === "true") {
-		likeBtns.innerHTML = "<img src='https://img.icons8.com/material-rounded/50/ffffff/facebook-like--v1.svg' onclick='like(false)' />"
-	} else {
-		likeBtns.innerHTML = "<img src='https://img.icons8.com/material-outlined/50/ffffff/facebook-like--v1.svg' onclick='like(true)' />"
-	}
-})
+// likeTitleWrapper.innerHTML = `<h2 class="like__title" data-title="${localStorage.getItem("liked") === "false" ? 'sure, like!' : 'already liked'}">Like my portfolio site</h2>`
+
+// window.addEventListener("load", async () => {
+
+// 	localStorage.setItem("liked", localStorage.getItem("liked") == null ? false : localStorage.getItem("liked"))
+
+// 	const headers = {
+// 		"X-Master-Key": KEY
+// 	}
+
+// 	await fetch("https://api.jsonbin.io/v3/b/631df3ec5c146d63ca979518/latest", { headers })
+// 		.then(res => res.json()).then(res => {
+// 			count = res.record.count
+// 		})
+
+// 	await fetch("https://api.jsonbin.io/v3/b/631f104f5c146d63ca98a112/latest", { headers })
+// 		.then(res => res.json()).then(async res => {
+// 			if (localStorage.getItem("visited") === "true") {
+// 				visitedCount.innerHTML = `Visited ${res.record.visited} times`
+// 			} else {
+// 				localStorage.setItem("visited", true)
+// 				visitedCount.innerHTML = `Visited ${res.record.visited + 1} times`
+// 				await fetch("https://api.jsonbin.io/v3/b/631f104f5c146d63ca98a112", {
+// 					method: 'PUT',
+// 					headers: {
+// 						"Content-Type": "application/json",
+// 						"X-Master-Key": KEY
+// 					},
+// 					body: JSON.stringify({ "visited": res.record.visited + 1 })
+// 				}).then(res => res.json()).then(res => console.log(res))
+// 			}
+// 		})
+
+// 	likeCount.innerHTML = count + " likes"
+
+// 	if (localStorage.getItem("liked") === "true") {
+// 		likeBtns.innerHTML = "<i class='fa-solid fa-heart like__btn' onclick='like(false)'></i>"
+// 	} else {
+// 		likeBtns.innerHTML = "<i class='fa-regular fa-heart like__btn' onclick='like(true)'></i>"
+// 	}
+// })
